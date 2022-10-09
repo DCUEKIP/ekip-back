ed9f9e3728710720c719f915b5d20404a966a1ed
Achille Bourgault <achille.bourgault@epitech.eu>
2022-10-09 16:14:42 +0100

    [+] Init Project

---
+++ src/main/java/com/dcuekip/ekipback/EkipBackApplication.java
@@ -0,0 +1,13 @@
+package com.dcuekip.ekipback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkipBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EkipBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dcuekip/ekipback/EkipBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcuekip.ekipback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkipBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
